26c8b41a23d514f18bfb9750742ead36174c24bf
«annetta_tok» <annetta.gri@mail.ru>
2023-02-19 17:56:35 +0300

    Init

---
+++ Task_2/Program.cs
@@ -0,0 +1,101 @@
+﻿/*
+ Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+Например, задан массив:
+
+1 4 7 2
+
+5 9 2 3
+
+8 4 2 4
+
+17 -> такого числа в массиве нет
+*/
+
+int[,] InitMatrix()
+{
+    int[,] array = new int[4, 4];
+
+    Random rnd = new Random();
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = rnd.Next(1, 10);
+        }
+    }
+
+    return array;
+}
+
+
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+
+        Console.WriteLine();
+    }
+}
+
+int GetNumber(string message)
+{
+    int result = 0;
+    while (true)
+    {
+        Console.WriteLine(message);
+
+        if (int.TryParse(Console.ReadLine(), out result))
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Ввели не число. Повторите ввод");
+        }
+    }
+
+    return result;
+}
+
+
+
+
+
+
+void GetFind(int[,] matrix, int number)
+{
+
+    for (int i = 1; i < matrix.GetLength(0); i++)
+    {
+
+        for (int j = 1; j < matrix.GetLength(1); j++)
+        {
+
+            if (matrix[i, j] == number)
+            {
+                Console.Write($"Такое число есть ");
+
+            }
+
+        }
+
+
+    }
+    Console.WriteLine($"Такого числа нет ");
+}
+
+int[,] matrix = InitMatrix();
+int number = GetNumber("Введите число ");
+
+PrintMatrix(matrix);
+
+GetFind(matrix, number);
+
+
---
+++ Task_3/Program.cs
@@ -0,0 +1,73 @@
+﻿/*
+Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+*/
+
+int ReadInt(string text)
+{
+    System.Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int[,] InitMatrix()
+{
+    int[,] array = new int[3, 4];
+
+    Random rnd = new Random();
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = rnd.Next(1, 10);
+        }
+    }
+
+    return array;
+}
+
+
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+
+        Console.WriteLine();
+    }
+}
+
+void Average(int[,] matr)
+{
+    int[] sum = new int[matr.GetLength(1)];
+
+    for (int i = 0; i < matr.GetLength(1); i++)
+    {
+        for (int j = 0; j < matr.GetLength(0); j++)
+        {
+            sum[i] += matr[j, i];
+        }
+    }
+    for (int i = 0; i < matr.GetLength(1); i++)
+    {
+        Console.Write(Math.Round(((double)sum[i] / matr.GetLength(0)), 1) + "\t");
+    }
+    System.Console.WriteLine();
+}
+
+
+int rows = ReadInt("Введите количество строк матрицы: ");
+int cols = ReadInt("Введите количество столбцов матрицы: ");
+int[,] matrix = InitMatrix();
+PrintMatrix(matrix);
+System.Console.WriteLine();
+Average(matrix);
